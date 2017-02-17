7c57983f3c2bada435a916b085d456cce629447b
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-02-17 16:31:20 +0100

    fix order by name

--- employee/employees.js
+++ employee/employees.js
@@ -8,7 +8,8 @@ function EmployeeService() {
 
 		Employee.find(function(err, employees) {
 			deferred.resolve(employees);
-	  });
+	  })
+		.sort( { lastname: 1 } );
 
 		return deferred.promise;
 	};
