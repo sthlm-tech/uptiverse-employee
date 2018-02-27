2429f7c149c43eef646433603e2dd720dd25cc5f
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2018-02-27 10:30:46 +0100

    removed url from config and added own db for employees

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 	port: 5000,
 	cacheEnabled: false,
 	cacheDuration: 3600000,
-	db_connectionString: 'mongodb://user-service-user:user-service-user@ds015636.mlab.com:15636/heroku_q9zz0x8s',
+	db_connectionString: process.env.MONGODB_URI || "",
 	communicator:{
 		path: process.env.COMMUNICATOR_PATH || "",
 		token: process.env.COMMUNICATOR_TOKEN || ""
